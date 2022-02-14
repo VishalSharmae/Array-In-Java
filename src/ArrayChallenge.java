28f5b26a956d1408b092e3a99af41a7c829fd889
zazyz <66414909+zazyz@users.noreply.github.com>
2022-02-14 23:54:39 +0530

    bug fixed and comments added

--- src/ArrayChallenge.java
+++ src/ArrayChallenge.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ArrayChallenge {
     public static Scanner scanner = new Scanner(System.in);
 
-
+// creating a different method to add two arrays in the returnMultiplication() method
     public static int[] addArray(int[] firstArray, int[] secondArray){
 
         int lengthArray1 = firstArray.length;
@@ -18,57 +18,63 @@ public class ArrayChallenge {
         return resultArray;
 
     }
+
     public static int returnMultiplications(int num){
-        boolean flag = true;
-        int count = 0;
+        boolean flag = true; // setting the true condition to run the loop
+        int count = 0; // count is the number of times multiplication will occur between the number and the random element
+                       // selected from the array created, basically it is how many times we will enter the loop.
 
         while (flag){
-        int tempNum= num;
-        int lengthOfNum = String.valueOf(num).length();
-        int[] firstArray = new int[lengthOfNum];
 
-        for (int i = 0; i < lengthOfNum; i++){
+            int tempNum= num;
+            int lengthOfNum = String.valueOf(num).length();
+            int[] firstArray = new int[lengthOfNum];
+
+            for (int i = 0; i < lengthOfNum; i++){ // getting the reversed array of the number
             firstArray[i] = tempNum % 10;
             tempNum /= 10;
-        }
+            }
 
-        int tempLength = lengthOfNum;
-        int[] reverseArray = new int[tempLength];
-        for (int i = 0; i < lengthOfNum ; i++){
-            reverseArray[tempLength-1] = firstArray[i];
-            tempLength--;
-        }
+            int tempLength = lengthOfNum;
 
+            int[] reverseArray = new int[tempLength];
+            for (int i = 0; i < lengthOfNum ; i++){ // reversing the obtained above array to get the desired array
+                reverseArray[tempLength-1] = firstArray[i];
+                tempLength--;
+            }
 
-
-            count++;
-            Random randomNumber = new Random();
+            count++; // incrementing the count because the number will be multiplied once below
+            Random randomNumber = new Random(); // using Random class from the java package
             int randomFromArray = reverseArray[randomNumber.nextInt(reverseArray.length)];
-            int newNum = (num * randomFromArray);
+            int newNum = (num * randomFromArray); //newNum is the new number obtained from multiplying the integer with random number
 
+            // converted the new obtained number into an array in the similar way above.
             int tempNum1= num;
             int lengthOfNum1 = String.valueOf(newNum).length();
             int[] repeatingArray = new int[lengthOfNum1];
 
-            for (int i = 0; i < lengthOfNum1; i++){
+            for (int i = 0; i < lengthOfNum1; i++){ // reversed array
                 repeatingArray[i] = tempNum1 % 10;
                 tempNum1 /= 10;
             }
 
             int tempLength1 = lengthOfNum1;
             int[] reverseArray1 = new int[tempLength1];
-            for (int i = 0; i < lengthOfNum1 ; i++){
+            for (int i = 0; i < lengthOfNum1 ; i++){ // correcting it
                 reverseArray1[tempLength1-1] = repeatingArray[i];
                 tempLength1--;
             }
             int[] resultArray = addArray(reverseArray,reverseArray1);
 
-            if (resultArray[lengthOfNum-1] == resultArray[lengthOfNum]){
-                flag = false;
-                // this will end the loop when we will get two same adjacent int values
+            for (int i = 0; i < resultArray.length-1; i++) { // fixed a bug here
+                if (resultArray[i] == resultArray[i+1]) { // checking is their any equal adjacent pairs
+                    flag = false;
+                    // this will end the loop when we will get two same adjacent int values
+                }
             }
+            // changing the value of num
             num = 0;
-            for (int i = 0; i < resultArray.length; i++){
+            for (int i = 0; i < resultArray.length; i++){ // converting the array to integer
                 num += resultArray[i] ;
                 if (i < resultArray.length -1){
                     num *=10;
